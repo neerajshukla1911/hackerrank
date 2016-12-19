69b04d68eeb3d3333850affa4bff96ca77720aed
Neeraj Shukla <neerajshukla1911@gmail.com>
2016-12-19 22:48:54 +0530

    palindrome inde

--- palindrome_index.py
+++ palindrome_index.py
@@ -4,18 +4,29 @@ PROBLEM STATEMENThttps://www.hackerrank.com/challenges/palindrome-index
 link: https://www.hackerrank.com/challenges/palindrome-index
 """
 import sys
-f = sys.stdin
-T = int(f.readline().strip())
-while T:
-    difference = lambda x, y: abs(ord(x) - ord(y))
-    s = f.readline().strip()
-    idx = -1
-    mid = len(s) // 2
-    l = zip(s, reversed(s))
-    for i in range(0, mid):
-        d = difference(l[i][0], l[i][1])
-        if d > 0:
-            idx = i
-            break
-    print idx
+
+
+def is_palindrome(s):
+    for idx in range(len(s) // 2):
+        if s[idx] != s[len(s) - idx - 1]:
+            return False
+    return True
+
+
+def palindrome_index(s):
+    for idx in range((len(s) + 1) // 2):
+        if s[idx] != s[len(s) - idx - 1]:
+            if is_palindrome(s[:idx] + s[idx + 1:]):
+                return idx
+            else:
+                return len(s) - idx - 1
+    return -1
+
+
+if __name__ == '__main__':
+    f = sys.stdin
+    T = int(f.readline())
+    while T:
+        s = input()
+        print(palindrome_index(s))
     T -= 1
